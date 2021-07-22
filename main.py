618bf35b87ac6254a0e853d27f0d5cb2de26965c
annasajkh <annaskherunas06@gmail.com>
2021-07-22 18:12:27 +0700

    2 attempt

--- main.py
+++ main.py
@@ -17,9 +17,42 @@ class Listener(tweepy.StreamListener):
         
         if status.favorited:
             return
-        
-        reply(twitter, status, memory)
+
         twitter.create_favorite(status.id)
+
+        name = status.user.screen_name
+
+        if not name in memory.keys():
+            memory[name] = []
+
+        memory[name].append(build_text(status))
+
+        if len(memory[name]) > 100:
+            memory[name].pop(0)
+        
+        if len(memory.keys()) > 100000:
+            del memory[memory.keys()[0]]
+        
+        text = "\n".join(memory[name]) + "\nBot: "
+
+        try:
+            result = asyncio.get_event_loop().run_until_complete(get_GPTJ(text))
+        except:
+            result = asyncio.get_event_loop().run_until_complete(get_GPTJ(text))
+
+        while is_bad(result):
+            try:
+                result = asyncio.get_event_loop().run_until_complete(get_GPTJ(text))
+            except:
+                result = asyncio.get_event_loop().run_until_complete(get_GPTJ(text))
+
+        memory[name].append(f"Bot: {result}")
+        
+        print("-" * 30)
+        print(text + result)
+        print("-" * 30)
+        
+        twitter.update_status(result, in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
 
     def on_error(self, status_code):
         if status_code == 420:
