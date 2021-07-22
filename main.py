f56add4b26aa84007ab0de1de2cc8ed6e3f14905
annasajkh <annaskherunas06@gmail.com>
2021-07-22 17:02:05 +0700

    2 attempt

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import traceback
 from twitter_api import *
 from helper import *
 
-import random
-
 memory = {}
 
 
@@ -33,25 +31,7 @@ stream = tweepy.Stream(auth, Listener())
 
 while True:
     try:
-        try:
-            stream.filter(track=["@TextSynth"], is_async=True)
-        except:
-            pass
-
-        try:
-            for tweet in tweepy.Cursor(twitter.home_timeline).items(100):
-                if tweet.favorited or tweet.user.screen_name == "TextSynth":
-                    continue
-
-                memory[tweet.user.screen_name] = []
-                reply(twitter, tweet, memory)
-                time.sleep(random.randrange(30,60))
-                tweet.favorite()
-        except:
-            pass
-
-        time.sleep(random.randrange(60 * 20, 60 * 30))
-
+        stream.filter(track=["@TextSynth"])
     except Exception as e:
         traceback.print_exc()
         time.sleep(10)
