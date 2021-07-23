66e3eafe7abcfd439116d87519f6b0601c024064
annasajkh <annaskherunas06@gmail.com>
2021-07-23 19:11:11 +0700

    test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import traceback
 from twitter_api import *
 from helper import *
 
+import random
 
     
 class Listener(tweepy.StreamListener):
@@ -17,6 +18,7 @@ class Listener(tweepy.StreamListener):
 
 
         reply(twitter, status)
+
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -30,7 +32,20 @@ stream = tweepy.Stream(auth, Listener())
 
 while True:
     try:
-        stream.filter(track=["@TextSynth"])
+        try:
+            stream.filter(track=["@TextSynth"])
+        except:
+            pass
+
+        try:
+            for status in tweepy.Cursor(twitter.home_timeline).items(20):
+                reply(twitter, status)
+        except:
+            time.sleep(10)
+            continue
+
+        time.sleep(random.randrange(60, 120) * 60)
+
     except Exception as e:
         traceback.print_exc()
         time.sleep(10)
