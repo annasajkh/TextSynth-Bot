e62c67ea565eb185138c38c87911ee0e5aad4504
annasajkh <annaskherunas06@gmail.com>
2021-07-24 14:27:20 +0700

    last

--- helper.py
+++ helper.py
@@ -49,6 +49,7 @@ async def get_gpt(text):
 
 
     text = filter(lambda x: x != "", [chunk.strip() for chunk in text.split("\n")])
+    print(text)
     text = "".join([json.loads(chunk)["text"] for chunk in text]).strip()
 
     return text
