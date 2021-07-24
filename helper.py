42ab98d074c6b8bcb5837cb02b4ce11417481afb
annasajkh <annaskherunas06@gmail.com>
2021-07-24 17:53:08 +0700

    now work

--- helper.py
+++ helper.py
@@ -101,7 +101,7 @@ async def reply(twitter, status):
     
     memory.reverse()
     
-    text =  ("TextSynth: Hello\n" + "\n".join(memory) + "\nTextSynth: ").replace(":", "")
+    text =  "TextSynth: Hello\n" + "\n".join(memory).replace(":", "") + "\nTextSynth: "
 
     result = await get_response(text)
 
