212b9988492d42f515cf7853f32acbce4632988b
annasajkh <annaskherunas06@gmail.com>
2021-07-24 15:18:24 +0700

    last

--- helper.py
+++ helper.py
@@ -18,6 +18,24 @@ paralleldots.set_api_key(os.environ["PARALLELDOTS_KEY"])
 
 url = "https://bellard.org/textsynth/api/v1/engines/gptj_6B/completions"
 
+def html_escape(text)
+    r = "";
+
+    for c in tex.split(""):
+        if c == "<":
+            r += "&lt;"
+        elif c == ">":
+            r += "&gt;"
+        elif c == "&":
+            r += "&amp;"
+        elif c == "\"":
+            r += "&quot;"
+        else:
+            r += c;
+        
+    return r;
+
+
 def build_text(status):
     text = get_text(status)
     text = re.sub("https://[^\s]+", "", text)
@@ -26,33 +44,39 @@ def build_text(status):
 
     return f"{status.user.screen_name}: {text}"
 
+
 def get_text(status):
     try:
         return status.extended_tweet["full_text"]
     except:
         return status.text
 
+
 async def get_gpt(text):
 
     payload = {
-        "prompt": rf"{text}",
+        "prompt": text.encode("utf-8").decode("utf-8").ignore,
         "temperature": 1.2,
         "top_k": 20, 
         "top_p": 0.8, 
         "seed": 0,
         "stream": True
     }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
     
     async with aiohttp.ClientSession() as session:
-        async with session.post(url,data=json.dumps(payload)) as response:
+        async with session.post(url,data=json.dumps(payload), headers=headers) as response:
             text = await response.text()
 
 
-    text = filter(lambda x: x != "", [chunk.strip() for chunk in text.split("\n")])
+    text = [chunk for chunk in text.split("\n\n")]
     print(list(text))
     text = "".join([json.loads(chunk)["text"] for chunk in text]).strip()
 
-    return rf"{text}"
+    return html_escape(text)
 
 async def get_response(text):
     result = await get_gpt(text)
