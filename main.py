fe26a557eaeadc8d2c7d78a91cf6a72e14c8a974
annasajkh <annaskherunas06@gmail.com>
2021-07-26 16:55:57 +0700

    follow

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import asyncio
 from twitter_api import *
 from helper import *
 
+dril_gpt2 = twitter.get_user("dril_gpt2").id_str
+make_up_a_guy_gpt = twitter.get_user("gpt2upaguy").id_str
+deep_leffen = twitter.get_user("DeepLeffen").id_str
+annas_virtual = twitter.get_user("AnnasVirtual").id_str
     
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
@@ -31,7 +35,7 @@ stream = tweepy.Stream(auth, Listener())
 while True:
     try:
         print("bot starting...")
-        stream.filter(track=["@TextSynth"])
+        stream.filter(track=["@TextSynth"], follow=[dril_gpt2, make_up_a_guy_gpt, deep_leffen, annas_virtual])
     except Exception as e:
         traceback.print_exc()
         time.sleep(10)
