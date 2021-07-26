66c45ed6455725b254ea296fe137067da1572ff8
annasajkh <annaskherunas06@gmail.com>
2021-07-26 17:02:41 +0700

    follow

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from helper import *
 dril_gpt2 = twitter.get_user("dril_gpt2").id_str
 make_up_a_guy_gpt = twitter.get_user("gpt2upaguy").id_str
 deep_leffen = twitter.get_user("DeepLeffen").id_str
-annas_virtual = twitter.get_user("AnnasVirtual").id_str
     
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
@@ -35,7 +34,7 @@ stream = tweepy.Stream(auth, Listener())
 while True:
     try:
         print("bot starting...")
-        stream.filter(track=["@TextSynth"], follow=[dril_gpt2, make_up_a_guy_gpt, deep_leffen, annas_virtual])
+        stream.filter(track=["@TextSynth"], follow=[dril_gpt2, make_up_a_guy_gpt, deep_leffen])
     except Exception as e:
         traceback.print_exc()
         time.sleep(10)
