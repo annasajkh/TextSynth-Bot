08e225c25e4aede800cea5e6125148eecd751cb7
annasajkh <annaskherunas06@gmail.com>
2021-07-27 00:05:07 +0700

    follow

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from helper import *
 dril_gpt2 = twitter.get_user("dril_gpt2").id_str
 make_up_a_guy_gpt = twitter.get_user("gpt2upaguy").id_str
 deep_leffen = twitter.get_user("DeepLeffen").id_str
+text_synth = twitter.get_user("TextSynth").id_str
     
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
@@ -19,12 +20,6 @@ class Listener(tweepy.StreamListener):
             return
 
         asyncio.get_event_loop().run_until_complete(reply(twitter, status))
-
-        if random.random() > 0.8:
-            text = asyncio.get_event_loop().run_until_complete(get_gpt(finetune + "\nBot: "))
-            text = re.split(".*?:",text)[0].strip()[:280]
-
-            twitter.update_status(text)
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -36,11 +31,10 @@ class Listener(tweepy.StreamListener):
 
 stream = tweepy.Stream(auth, Listener())
 
-
 while True:
     try:
         print("bot starting...")
-        stream.filter(track=["@TextSynth"], follow=[dril_gpt2, make_up_a_guy_gpt, deep_leffen])
+        stream.filter(track=["@TextSynth"], follow=[dril_gpt2, make_up_a_guy_gpt, deep_leffen, text_synth])
     except Exception as e:
         traceback.print_exc()
         time.sleep(10)
