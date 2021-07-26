c9ac55de314351bf7f1070cba0fa01f636963f65
annasajkh <annaskherunas06@gmail.com>
2021-07-27 00:45:46 +0700

    follow

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ class Listener(tweepy.StreamListener):
         if status.user.screen_name == "TextSynth":
             return
 
-        asyncio.get_event_loop().run_until_complete(reply(twitter, status))
-
         if random.random() > 0.8:
             text = asyncio.get_event_loop().run_until_complete(get_gpt(finetune + "\nBot: "))
             text = re.split(".*?:",text)[0].strip()[:280]
