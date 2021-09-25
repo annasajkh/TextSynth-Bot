a8f9a1d2f40d94def19f40521dbd364ac3b1c7fa
Annas <61279556+annasajkh@users.noreply.github.com>
2021-09-25 05:12:53 +0000

    change things

--- setup.py
+++ setup.py
@@ -29,6 +29,5 @@ if anyone feels like getting a snack after this shit post please do#
 i've lost faith in my own opinions#
 wtf? i just farted#
 the rain is pretty but i don't wanna go outside and be wet#
-yumyum i want to make baby with you#
 i was doing dishes when i accidentally dropped the bowl, hard!#
 """.strip()
