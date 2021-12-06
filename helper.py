1aeee3df5b10bed35436a8447888a354a77b74a5
Annas <61279556+annasajkh@users.noreply.github.com>
2021-12-06 04:17:38 +0000

    stupid mistake

--- helper.py
+++ helper.py
@@ -31,8 +31,8 @@ def get_text(status):
     except:
         return status.text
 
-
-async def get_gpt(text, session):
+#async
+def get_gpt(text):#, session):
     # payload = {
     #     "prompt": text,
     #     "temperature": 0.7,
@@ -97,7 +97,7 @@ async def get_gpt(text, session):
 
 def get_response(text, memory, session, loop):
     #result = loop.run_until_complete(get_gpt(text, session))
-    result = get_gpt(text, session)
+    result = get_gpt(text)#, session)
     # result = re.split(".*:",result)[0].strip()[:280]
     # result = re.sub("\n", " ", result)
 
